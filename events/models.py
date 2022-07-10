923ce67f84d24606bfed05516f120ec632109116
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-10 21:45:41 +0300

    Admin settings, admin event model & Bug fix

--- events/models.py
+++ events/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class EventsConfig(models.Model):
+class Events(models.Model):
     event_img = models.ImageField(upload_to='event_images/')
     event_text = models.CharField(max_length=300)
