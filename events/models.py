d7a84a54ce3d7fdc6d4f3e2523636dd8c335ab59
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-10 20:30:46 +0300

    Create apps and model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class EventsConfig(models.Model):
+    event_img = models.ImageField(upload_to='event_images/')
+    event_text = models.CharField(max_length=300)
