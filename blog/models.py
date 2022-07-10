3d6293ecde38b27114a947b241813214d7d35159
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-10 21:50:43 +0200

    Create article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_title = models.TextField(max_length=30)
+    article_date = models.DateField()
+    article_text = models.CharField(max_length=500)
+    article_img = models.ImageField('blog_images/')
--- events/admin.py
+++ events/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Events
 
+
 admin.site.register(Events)
