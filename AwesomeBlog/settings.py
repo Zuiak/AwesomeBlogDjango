05bdea7a132eb882864fdaf5385b5ec4e42a1567
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-10 21:41:38 +0200

    Move from SQLite3 to Postgresql

--- AwesomeBlog/settings.py
+++ AwesomeBlog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'AwesomeBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
