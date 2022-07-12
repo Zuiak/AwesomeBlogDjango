f924b92cde6c4e2fdf5f6a39e5607d1a8be0b051
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-12 08:40:53 +0200

    Create blog page

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class Article(models.Model):
     article_title = models.TextField(max_length=30)
     article_date = models.DateField()
-    article_text = models.CharField(max_length=500)
+    article_text = models.CharField(max_length=10000000)
     article_img = models.ImageField('blog_images/')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Article
+
 
 # Create your views here.
+def blog(request):
+    posts = Article.objects
+    return render(request, 'blog/blog.html', {'article': posts})
